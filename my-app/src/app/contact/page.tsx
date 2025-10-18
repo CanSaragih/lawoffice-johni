d52f5c82b079a03078c7e27ea900e5581a472e1a
CanSaragih <canwhardana@gmail.com>
2025-10-18 09:42:00 +0700

    feat: add embedding maps in contact page

--- my-app/src/app/contact/page.tsx
+++ my-app/src/app/contact/page.tsx
@@ -1,16 +1,20 @@
 import PageHeader from "@/components/PageHeader";
-import ContactPage from "@/components/section/Contact";
+import EmbeddingMaps from "@/components/ui/contact/EmbeddingMaps";
 import FormContact from "@/components/ui/contact/FormContact";
+import GridContact from "@/components/ui/contact/GridContact";
 
 export default function Contact() {
   return (
     <div className="mt-17 md:mt-22">
       <PageHeader title="Contact Us" bgImage="/img/header.jpg" />
-      <div>
-        <ContactPage />
+      <div className="mb-10 md:mb-15 mt-10 md:mt-15">
+        <GridContact />
+      </div>
+      <div className="mb-5 md:mb-10">
+        <FormContact />
       </div>
       <div className="mb-10 md:mb-15">
-        <FormContact />
+        <EmbeddingMaps />
       </div>
     </div>
   );
--- my-app/src/components/ui/contact/GridContact.tsx
+++ my-app/src/components/ui/contact/GridContact.tsx
@@ -2,28 +2,30 @@ import contactData from "@/data/contactData";
 
 export default function GridContact() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 py-8 sm:py-10 px-4 sm:px-0">
-      {contactData.map((item, index) => (
-        <div
-          key={index}
-          className="flex flex-col items-center justify-center bg-white p-6 sm:p-8 md:p-10 rounded-xl  shadow-[0_0_10px_rgba(0,0,0,0.04),0_0_25px_rgba(0,0,0,0.06)] hover:shadow-[0_0_30px_rgba(0,0,0,0.10)] transition-shadow duration-300"
-        >
-          {/* Ikon dalam lingkaran abu muda */}
-          <div className="flex items-center justify-center w-14 h-14 sm:w-16 sm:h-16 md:w-20 md:h-20 bg-gray-100 rounded-full mb-5 sm:mb-6">
-            <item.icon className="w-8 h-8 sm:w-10 sm:h-10 text-gray-600" />
-          </div>
+    <section className="max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 py-8 sm:py-10 px-4 sm:px-0">
+        {contactData.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center bg-white p-6 sm:p-8 md:p-10 rounded-xl  shadow-[0_0_10px_rgba(0,0,0,0.04),0_0_25px_rgba(0,0,0,0.06)] hover:shadow-[0_0_30px_rgba(0,0,0,0.10)] transition-shadow duration-300"
+          >
+            {/* Ikon dalam lingkaran abu muda */}
+            <div className="flex items-center justify-center w-14 h-14 sm:w-16 sm:h-16 md:w-20 md:h-20 bg-gray-100 rounded-full mb-5 sm:mb-6">
+              <item.icon className="w-8 h-8 sm:w-10 sm:h-10 text-gray-600" />
+            </div>
 
-          {/* Judul */}
-          <h3 className="text-lg sm:text-xl md:text-2xl font-serif text-gray-800 mb-2 sm:mb-3 text-center">
-            {item.title}
-          </h3>
+            {/* Judul */}
+            <h3 className="text-lg sm:text-xl md:text-2xl font-serif text-gray-800 mb-2 sm:mb-3 text-center">
+              {item.title}
+            </h3>
 
-          {/* Deskripsi */}
-          <div className="text-center text-gray-600 leading-relaxed text-sm sm:text-[15px]">
-            {item.desc}
+            {/* Deskripsi */}
+            <div className="text-center text-gray-600 leading-relaxed text-sm sm:text-[15px]">
+              {item.desc}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </section>
   );
 }
